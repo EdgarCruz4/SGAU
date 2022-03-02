74662a8252bb3d5e953dc4549421f9d0c3d68da5
Edgar Cruz <86249936+EdgarCruz4@users.noreply.github.com>
2022-03-02 15:42:15 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    
+    <!-- Los iconos tipo Solid de Fontawesome-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
+    <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+
+    <!--Css personalizado-->
+    <link rel="stylesheet" href="Frontend/css/start_window.css">
+    <title>Inicio de sesión</title>
+</head>
+<body>
+    <div>
+        <div>
+            <div>
+                <form action="" action="">
+                    <div>
+                        <button class="btn btn-primary"><a href="">Alumnos</a></button>
+                        <button class="btn btn-success"><a href="">Docentes</a></button><br>
+                        <p>¿No tienes cuenta? <a href="Frontend/Sing_Up.php">Registrate aqui</a></p>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
