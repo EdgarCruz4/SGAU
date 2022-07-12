edf22ead07b65ab7fb4db401f080e8d43f7c0bb5
Neitzer <olguinneitzer@gmail.com>
2022-07-11 19:22:30 -0500

    Actualización SEM 1
    Diseño realizado en la primera semana

--- index.php
+++ index.php
@@ -13,20 +13,59 @@
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
     <!--Css personalizado-->
-    <link rel="stylesheet" href="Frontend/css/start_window.css">
+    <link rel="stylesheet" href="Frontend/css/index.css">
     <title>Inicio de sesión</title>
 </head>
 <body>
     <div>
         <div>
-            <div>
-                <form action="" action="">
-                    <div>
-                        <button class="btn btn-primary"><a href="Frontend/Sing_in_almn.php">Alumnos</a></button>
-                        <button class="btn btn-success"><a href="Frontend/Sing_in_prfsr.php">Docentes</a></button><br>
-                        <p>¿No tienes cuenta? <a href="Frontend/Sing_Up.php">Registrate aqui</a></p>
+                <main>
+                    <div class="row">
+                        <div class="col">
+                        <span class="float-center"><h1>Pantallas de usuario</h1></span>
+                        </div>
+                        <div class="col">
+                        <span class="float-right"><button type="button" class="btn btn-danger" style="float-right"> < Regresar</button></span>
+                        </div>
                     </div>
-                </form>
+                </main>
+
+            <div class="formulario_content">
+                <div class="formulario">
+                    <form action="" action="">
+                        <div>
+                            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                            <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            </div>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                <img src="Frontend\img\2.png" class="d-block w-100" alt="..." height="400px">
+                                </div>
+                                <div class="carousel-item">
+                                <img src="Frontend\img\2.png" class="d-block w-100" alt="..." height="400px">
+                                </div>
+                                <div class="carousel-item">
+                                <img src="Frontend\img\descarga.png" class="d-block w-100" alt="...">
+                                </div>
+                            </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Anterior</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Siguiente</span>
+                            </button>
+                            </div>
+                            <button class="btn btn-primary"><a href="Frontend/Sing_in_almn.php">Alumnos</a></button>
+                            <button class="btn btn-success"><a href="Frontend/Sing_in_prfsr.php">Docentes</a></button><br>
+                            <p>¿No tienes cuenta? <a href="Frontend/Sing_Up.php">Registrate aqui</a></p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
