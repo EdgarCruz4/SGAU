aca1f1f4ed2a6cb673ac96b547ae3d9a34f73eb5
Neitzer <olguinneitzer@gmail.com>
2022-08-10 16:47:05 -0500

    Login de alumno y docente
    diseño de login de alumno y docente

--- Frontend/Sing_in_almn.php
+++ Frontend/Sing_in_almn.php
@@ -13,30 +13,47 @@
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
     
     <!--Css personalizado-->
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/alumno.css">
 
     <title>Inicio de sesión</title>
 </head>
-<body>
+<body class="align">
+<div class="login">
+
+    <header class="login__header">
+      <h2><svg class="icon">
+          <use xlink:href="#icon-lock" />
+        </svg>Inicio de sesión</h2>
+    </header>
+
+    <form action="../Backend/usu_vrf_std.php" class="login__form" method="POST">
+
+      <div>
+        <label for="text">Matricula</label>
+        <input type="text" id="matricula" name="enrollment" placeholder="Matricula" minlength="7" maxlength="7" required>
+      </div>
+
+      <div>
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" name="password" placeholder="Contraseña">
+      </div>
+
+      <div>
+      <input class="button" type="submit" value="Ingresar">
+      </div>
     <div>
-        <div>
-            <div>
-                <form method="POST" action="../Backend/usu_vrf_std.php">
-                    <div>
-                        <input type="text" placeholder="Matricula" name="enrollment" minlength="7" maxlength="7" required>
-                    </div>
-                    <div>
-                        <input type="password" placeholder="Contraseña" name="password" required>
-                    </div>
-                    <div>
-                        <button class="btn btn-primary"><i class="fas fa-sign-in-alt"></i> Ingresar</button>
-                    </div>
-                    <div>
-                        <a href="../index.php">Regresar a la pantalla de inicio</a></p>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <a class="return" href="../index.php">Regresar a la pantalla de inicio</a></p>
     </div>
+    </form>
+
+  </div>
+
+  <svg xmlns="http://www.w3.org/2000/svg" class="icons">
+
+    <symbol id="icon-lock" viewBox="0 0 448 512">
+      <path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z" />
+    </symbol>
+
+  </svg>
 </body>
 </html>
--- Frontend/Sing_in_dcnt.php
+++ Frontend/Sing_in_dcnt.php
@@ -13,30 +13,47 @@
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
     
     <!--Css personalizado-->
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/dcnt.css">
 
     <title>Inicio de sesión profesores</title>
 </head>
-<body>
+<body class="align">
+<div class="login">
+
+    <header class="login__header">
+      <h2><svg class="icon">
+          <use xlink:href="#icon-lock" />
+        </svg>Inicio de sesión</h2>
+    </header>
+    
+    <form form method="POST"  class="login__form" action="../Backend/usu_vrf_dcnt.php">
+
+      <div>
+        <label for="text">NFC</label>
+        <input type="text" id="nfc" name="enrollment" placeholder="NFC" minlength="13" maxlength="13" required>
+      </div>
+
+      <div>
+        <label for="password">Contraseña</label>
+        <input type="password" id="password" name="password" placeholder="Contraseña">
+      </div>
+
+      <div>
+      <input class="button" type="submit" value="Ingresar">
+      </div>
     <div>
-        <div>
-            <div>
-                <form method="POST" action="../Backend/usu_vrf_dcnt.php">
-                    <div>
-                        <input type="text" name="nfc" placeholder="NFC" minlength="13" maxlength="13" required>
-                    </div>
-                    <div>
-                        <input type="password" name="password" placeholder="Contraseña" required>
-                    </div>
-                    <div>
-                        <button class="btn btn-primary"><i class="fas fa-sign-in-alt"></i> Ingresar</button>
-                    </div>
-                    <div>
-                        <a href="../index.php">Regresar a la pantalla de inicio</a></p>
-                    </div>
-                </form>
-            </div>
-        </div>
+    <a class="return" href="../index.php">Regresar a la pantalla de inicio</a></p>
     </div>
+    </form>
+
+  </div>
+
+  <svg xmlns="http://www.w3.org/2000/svg" class="icons">
+
+    <symbol id="icon-lock" viewBox="0 0 448 512">
+      <path d="M400 224h-24v-72C376 68.2 307.8 0 224 0S72 68.2 72 152v72H48c-26.5 0-48 21.5-48 48v192c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V272c0-26.5-21.5-48-48-48zm-104 0H152v-72c0-39.7 32.3-72 72-72s72 32.3 72 72v72z" />
+    </symbol>
+
+  </svg>
 </body>
 </html>
